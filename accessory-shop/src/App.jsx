05ae2d49fc717241d3a29da15466dc3cce500f6e
Minthuka <u6530143@au.edu>
2024-07-25 23:32:17 +0700

    New Push

--- accessory-shop/src/App.jsx
+++ accessory-shop/src/App.jsx
@@ -1,86 +1,116 @@
-import { useState , useRef} from 'react'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import { Button, Container, Row,Col} from 'react-bootstrap'
-import { Form } from 'react-bootstrap'
-import productList from './accessory-products.json'
-import DataTable from './components/DataTable'
+import React, { useState, useRef } from 'react';
+import { Button, Container, Row, Col, Form } from 'react-bootstrap';
+import productList from './accessory-products.json';
+import DataTable from './components/DataTable';
 
-function App() { 
+function App() {
+  const pRef = useRef();
+  const qRef = useRef();
+  const [price, setPrice] = useState(productList[0].price);
+  const [selectedItems, setSelectedItems] = useState([]);
+  const [filteredSelectedItems, setFilteredSelectedItems] = useState([]);
 
-  const pRef = useRef()
-  const qRef = useRef()
-  const [price,setPrice] = useState(productList[0].price)
+  const deleteItemByIndex = (index) => {
+    selectedItems.splice(index, 1);
+    setSelectedItems([...selectedItems]);
+    setFilteredSelectedItems([...selectedItems]);
+  };
 
-  const [selectedItems,setSelectedItems]= useState([])
+  const filter = (keyword) => {
+    if (keyword.trim() === '') {
+      setFilteredSelectedItems([...selectedItems]);
+    } else {
+      const filteredItems = selectedItems.filter((item) =>
+        item.name.toLowerCase().includes(keyword.toLowerCase())
+      );
+      setFilteredSelectedItems(filteredItems);
+    }
+  };
 
+  const sort = (direction) => {
+    const sortedItems = [...filteredSelectedItems];
+    sortedItems.sort((a, b) => {
+      if (a.name < b.name) {
+        return direction === 'asc' ? -1 : 1;
+      }
+      if (a.name > b.name) {
+        return direction === 'asc' ? 1 : -1;
+      }
+      return 0;
+    });
+    setFilteredSelectedItems(sortedItems);
+  };
 
+  const handleAdd = () => {
+    const pid = pRef.current.value;
+    const product = productList.find((p) => p.id == pid);
+    const q = qRef.current.value;
 
-  const handleAdd = (e) => {
-    
-    const pid = pRef.current.value 
-    const product = productList.find(p => p.id == pid)
-    const q = qRef.current.value
-    console.log(pid,q)
     selectedItems.push({
-      //id: product.id ,
-      //name: product.name ,
-      //price: product.price , 
       ...product,
-      qty: q 
-    })
-      console.table(selectedItems)
-      setSelectedItems([...selectedItems])
-  }
+      qty: q,
+    });
+    console.table(selectedItems);
+    setSelectedItems([...selectedItems]);
+    setFilteredSelectedItems([...selectedItems]);
+  };
+
   const handleProductChanged = (e) => {
-    const pid = e.target.value
-    const product = productList.find(p => p.id == pid)
-    const p = product.price
-    console.log(p)
-    setPrice(p)
-  }
+    const pid = e.target.value;
+    const product = productList.find((p) => p.id == pid);
+    setPrice(product.price);
+  };
 
   return (
-    <>
     <Container>
-    <Row>
-      <Col xs={2}>
-        <span> Product</span>
+      <Row>
+        <Col xs={2}>
+          <span>Product: </span>
         </Col>
         <Col>
           <Form.Select ref={pRef} onChange={handleProductChanged}>
-  {
-    productList.map((p) => (
-      <option key={p.id} value={p.id}>{p.name}</option>
-    ))
-  }
+            {productList.map((p) => (
+              <option key={p.id} value={p.id}>
+                {p.name}
+              </option>
+            ))}
           </Form.Select>
-      </Col>
-      <Row>
+        </Col>
       </Row>
-      <Col xs = {2}>
-          Price:
+      <Row>
+        <Col xs={2}>Price:</Col>
+        <Col>{price}</Col>
+      </Row>
+      <Row>
+        <Col xs={2}>
+          <span>Quantity: </span>
         </Col>
         <Col>
-          {price}
-      </Col>
-    </Row>
-    <Row>
-      <Col xs={2}>
-        <span> Quantity</span>
-      </Col>
-      <Col>
-      <Form.Control type="number" ref={qRef} onChange={handleAdd} />
-      </Col>
-    </Row>
-      <Button variant = "primary" onClick={handleAdd}> Add</Button>
+          <input type="text" ref={qRef} defaultValue={1} />
+        </Col>
+      </Row>
+      <Button variant="info" onClick={handleAdd}>
+        Add
+      </Button>
+      <Row className="mt-3">
+        <Col xs={6}>
+          <Button variant="light" onClick={() => sort('asc')}>
+            Sort A-Z <i className="bi bi-arrow-up"></i>
+          </Button>
+          <Button variant="light" onClick={() => sort('desc')}>
+            Sort Z-A <i className="bi bi-arrow-down"></i>
+          </Button>
+        </Col>
+      </Row>
 
-      <DataTable data = {selectedItems} />
-
+      <DataTable
+        data={filteredSelectedItems}
+        onDelete={deleteItemByIndex}
+        onFilter={filter}
+        onSort={sort}
+      />
     </Container>
-    </>
-  )
- 
+  );
+}
 
-} 
-
-export default App 
+export default App;
--- accessory-shop/src/components/DataTable.jsx
+++ accessory-shop/src/components/DataTable.jsx
@@ -1,28 +1,52 @@
-import React from 'react';
-import Table from 'react-bootstrap/table'
+import React, { useState, useRef } from 'react';
+import { Container, Table, Button } from 'react-bootstrap';
 
-const DataTable = ({ data }) => {
+const DataTable = ({ data, onDelete, onFilter }) => {
+    const sRef = useRef();
+
+    const handleDelete = (index) => {
+        console.debug('Delete', index);
+        onDelete(index);
+    };
+
+    const handleSearch = () => {
+        console.log('Search', sRef.current.value)
+        const keyword = sRef.current.value
+        onFilter(keyword)
+    };
+
     return (
-        <Table striped bordered hover variant='dark'>
-            <thead>
-                <tr>
-<th>Id</th>
-<th>Product Name</th>
-<th>Price</th>
-<th>Qty</th>
-                </tr>
-            </thead>
-            <tbody>
-                {data.map((item, index) => (
-                    <tr key={index}>
-                       <td>{item.id}</td>
-                       <td>{item.name}</td>
-                       <td>{item.price}</td>
-                       <td>{item.qty}</td>
+        <Container>
+            <input type="text" placeholder="Search..." ref={sRef} />{' '}
+            <Button onClick={handleSearch} 
+            variant="outline-primary">
+                <i className="bi bi-search"></i>Search</Button>
+            <Table striped bordered hover variant="dark">
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>Product Name</th>
+                        <th>Price</th>
+                        <th>Qty</th>
                     </tr>
-                ))}
-            </tbody>
-        </Table>
+                </thead>
+                <tbody>
+                    {data.map((item, index) => (
+                        <tr key={index}>
+                            <td>
+                                <i
+                                    className="bi bi-trash"
+                                    onClick={() => handleDelete(index)}
+                                ></i>
+                            </td>
+                            <td>{item.name}</td>
+                            <td>{item.price}</td>
+                            <td>{item.qty}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </Table>
+        </Container>
     );
 };
 
